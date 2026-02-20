70b6b200b1c7c78807b47de6c786d981b4de33aa
ge94caf <ge94caf>
2026-02-21 06:39:44 +0800

    try2

--- verification/error.py
+++ verification/error.py
@@ -1,10 +1,6 @@
-IMEM[0] written = 0x42000437
-IMEM[0] readback = 0x0
 DMEM[0x100] readback = 0x12345678
 GPIO2=0 readback: 0x0
 GPIO2=1 readback: 0x1
 DONE_ADDR = 0x42001ffc
-[TIMEOUT] Mem=0x0 HW=0
-Result[0..7] = [0, 0, 0, 0, 0, 0, 0, 0]
-Expect      = [1, 2, 3, 4, 5, 7, 8, 9]
-FAIL
+IMEM[0] got 0x42002e37
+[FAIL] mini done store not observed, flag= 0x0
